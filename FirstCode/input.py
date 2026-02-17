5701728a5fc07f385bcf60f34f78f4a9897b3d09
Manish Kumar Gupta <manishbtt9336@gmail.com>
2026-02-17 22:32:41 +0530

    Take input in python

---
+++ FirstCode/input.py
@@ -0,0 +1,7 @@
+name = input("Enter name: ") #String input
+age = int(input("Enter age: "))  # integer input
+percentage = float(input("Enter marks: ")) # Decimal point input
+
+print(f"Name- {name}")
+print(f"Age- {age}")
+print(f"Marks- {percentage}")
