25a6938eb4b759b24ee97e12651c5259aa101d42
Manish Kumar Gupta <manishbtt9336@gmail.com>
2026-02-18 23:03:43 +0530

    Data type casting

---
+++ FirstCode/variableCast.py
@@ -0,0 +1,13 @@
+a = "10"
+a1 = int(a) #casting from string to integer
+
+b = 5
+b1 = float(b) #integer to float
+
+c = 25
+c1 = str(c) #integer to string
+
+print(a1)
+print(b1)
+print(c1)
+print(type(a1)) #with this line, we find which type of data is
