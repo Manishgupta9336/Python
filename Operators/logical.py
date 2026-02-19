266953b9a49c0964c86bf3e682421dabff6dc789
Manish Kumar Gupta <manishbtt9336@gmail.com>
2026-02-19 22:49:09 +0530

    Logical operators

---
+++ Operators/logical.py
@@ -0,0 +1,5 @@
+a = True
+b = False
+print(a and b) # true if both true
+print(a or b) # true if 1 true
+print(not a) # true if a False 
---
+++ Operators/relational.py
@@ -0,0 +1,9 @@
+a = 10
+b = 5
+
+print(a==b) #Equal
+print(a!=b) #Not  equal
+print(a>b) # a is greater than b
+print(a<b) #b is greater than a
+print(a>=b)
+print(a<=b)  
