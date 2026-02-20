0d00bb51dfbeac6b8a657d47716769fc6c864c5d
Manish Kumar Gupta <manishbtt9336@gmail.com>
2026-02-20 22:42:13 +0530

    Data types

---
+++ FirstCode/dataType.py
@@ -0,0 +1,13 @@
+a = 5
+pi = 3.14
+c = "Cat"
+b = True
+list1 = [1,2,3,"manish"]
+tuple1 = (2,3,4,5,6)
+
+print(type(a))
+print(type(pi))
+print(type(c))
+print(type(b))
+print(type(list1))
+print(type(tuple1))
