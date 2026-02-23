8531e09da38bcaee4ce781a1811ecfc6409bc32d
Manish Kumar Gupta <manishbtt9336@gmail.com>
2026-02-23 21:38:34 +0530

    basic methods of string

---
+++ String/methods.py
@@ -0,0 +1,7 @@
+a = "Hello World"
+print(a.capitalize()) #capitalized 1st letter
+print(a.upper()) #convert words into capital letter
+print(a.lower()) #convert words into lower letter
+print(a.split()) #split into  list
+print(a.find("o")) #find index of first "o" in str
+print(a.count("l")) #count the "l" in str
