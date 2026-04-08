672e07690e8cf4d1aa3031b71e0fd2438a2c1de3
Manish Kumar Gupta <manishbtt9336@gmail.com>
2026-04-08 19:10:41 +0530

    relational operators

--- Operators/relational.py
+++ Operators/relational.py
@@ -5,5 +5,5 @@ print(a==b) #Equal
 print(a!=b) #Not  equal
 print(a>b) # a is greater than b
 print(a<b) #b is greater than a
-print(a>=b)
-print(a<=b)  
+print(a>=b) # a is greater than or equal to b
+print(a<=b)  # a is less than or equal to b
