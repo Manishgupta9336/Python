604f7b9cabd3ec1766c18795ee4a679d25f22c73
Manish Kumar Gupta <manishbtt9336@gmail.com>
2026-04-08 19:09:21 +0530

    Basic calculator

--- Operators/calculator.py
+++ Operators/calculator.py
@@ -1,6 +1,6 @@
 a = float(input("Enter 1st number: "))
 b = float(input("Enter 2nd number: "))
-op = input("Choose a operator '+ - * / ' ")
+op = input("Choose a operator ( + - * / ) ")
 
 if(op=="+"):
     print("Add- ",a+b)
