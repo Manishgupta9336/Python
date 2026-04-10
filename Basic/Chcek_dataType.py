5908f5b90d9fecdfb9d66159d4914db9a79b5cf6
Manish Kumar Gupta <manishbtt9336@gmail.com>
2026-04-10 21:59:10 +0530

    Check data types

--- Basic/Chcek_dataType.py
+++ Basic/Chcek_dataType.py
@@ -3,7 +3,8 @@ b = 3.14
 c = True
 d = "Hello"
 n = (1,5,3)
-m = [4,"hii",30]
+m = [4,"hii",30,]
+s = {1,2,3,4,5}
 
 print(type(a))
 print(type(b))
@@ -11,3 +12,4 @@ print(type(c))
 print(type(d))
 print(type(n))
 print(type(m))
+print(type(s))
